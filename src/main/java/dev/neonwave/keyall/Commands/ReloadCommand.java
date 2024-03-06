2426cecb1b8635be6eddcf1eff4813e22681b920
Ammar <123098233+Ammar-XD@users.noreply.github.com>
2024-03-06 20:03:32 +0530

    Update ReloadCommand.java

--- src/main/java/dev/neonwave/keyall/Commands/ReloadCommand.java
+++ src/main/java/dev/neonwave/keyall/Commands/ReloadCommand.java
@@ -10,18 +10,21 @@ import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String cmd, @NotNull String[] args) {
-        if (cmd.equalsIgnoreCase("keyall") && args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (label.equalsIgnoreCase("keyall") && args.length > 0 && args[0].equalsIgnoreCase("reload")) {
             try {
                 Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin("KeyAll"));
                 Bukkit.getPluginManager().enablePlugin(Bukkit.getPluginManager().getPlugin("KeyAll"));
-                if  (sender instanceof Player)  {
-                    Player  player = (Player) sender;
-                    player.sendMessage(ChatColor.GREEN + "KeyAll has been restarted");
+
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    player.sendMessage(ChatColor.GREEN + "KeyAll has been reloaded");
                 }
+
+                return true; // Indicate successful handling of the command
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                e.printStackTrace(); // Print stack trace for debugging purposes
+                return false; // Indicate failure
             }
         }
         return false;
